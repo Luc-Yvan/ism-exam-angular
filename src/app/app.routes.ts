00b2e9e10b02f94541f49e29f4feb6019c010867
Jawk <jasonawume5@gmail.com>
2024-05-14 17:40:42 +0000

    Inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProfComponent } from './core/pages/prof/prof.component';
 import { SalleComponent } from './core/pages/salle/salle.component';
 import { FormSalleComponent } from './core/pages/form.salle/form.salle.component';
 import { SessionComponent } from './core/pages/session/session.component';
+import { InscritsComponent } from './core/pages/inscrits/inscrits.component';
 
 export const routes: Routes = [
     {
@@ -22,6 +23,10 @@ export const routes: Routes = [
     {
         path:"session/:id",
         component:SessionComponent
+    },
+    {
+        path:"inscription/:id",
+        component:InscritsComponent
     },
     {
         path:"salle",
